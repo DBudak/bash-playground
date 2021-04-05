bd05f1952bcc7731b8820bbef719dcdec6ab4709
dbudak <dmitriybudak@gmail.com>
2021-04-05 00:56:15 -0500

    more file management

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # shell-playground
 
-A personal collection of useful shell commands
+A personal collection of useful shell commands.
+It is by no means extensive. For extensive guides use man pages and $<command> --help.
--- unix/file_management.sh
+++ unix/file_management.sh
@@ -8,13 +8,13 @@ $ touch <name> <name2> <name3> # create file
 $ touch <filename> 
 
 $ mkdir <name> # create directory
- -p <a/b/c>    # create a hierarchy of directories
- -v            # verbose
- -m a=rwx      # sets access for all users
+ -p <a/b/c> # create a hierarchy of directories
+ -v         # verbose
+ -m a=rwx   # sets access for all users
 
 $ stat <name> # get information about file or directory
 
-$ rm # remove files or directories
+$ rm <name> # remove files or directories
  -i  # prompt before every removal
  -I  # prompt beofre removing over 3 files
  -r  # recursive
@@ -23,9 +23,20 @@ $ rm # remove files or directories
 # Optimal:
 $ rm -I
 
-$ rmdir # remove a directory
+$ rmdir # remove an empty directory
+ -p <a/b/c> # remove directory and its parents
+ -v         # verbose
 
 $ cp <what> <where> # copy file or directory
+ -b # make a backup
+ -i # prompt before overwrite
+ -l # hard link files instead of copying
+ -n # do not overwrite, ignores -i
+ -r # recursive
+ -u # overwrite only when souce is newer
+ -v # verbose
+# Optimal:
+$ cp -i <what> <where> 
 
 $ mv <what> <where> # move file or directory
 
